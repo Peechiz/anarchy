213f7e7a860841355ba09e3db87ad1c13ff6b959
Chris Impicciche <peechiz@bakudan.local>
2016-08-31 18:01:37 -0500

    adds skaters migration

---
+++ migrations/20160831224920-skaters.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable(
+      'skaters',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        createdAt: {
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          type: Sequelize.DATE
+        },
+        userName: Sequelize.STRING,
+        derbyName: Sequelize.STRING,
+        number: Sequelize.STRING,
+        team: Sequelize.STRING,
+        favPosition: Sequelize.STRING,
+        photo: Sequelize.STRING,
+        admin: Sequelize.BOOLEAN,
+        summary: Sequelize.TEXT,
+      }
+    )
+  },
+
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('skaters');
+  }
+};
