421c0b63b6fefa230476a0a9bda24ad058f09a6c
Chris Impicciche <peechiz@bakudan.local>
2016-09-01 10:35:55 -0500

    removes comments, tweaks formatting

--- migrations/20160831225755-gear.js
+++ migrations/20160831225755-gear.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.createTable('gear', {
       id: {
         type: Sequelize.INTEGER,
@@ -25,7 +18,7 @@ module.exports = {
     })
   },
 
-  down: function (queryInterface, Sequelize) {    return queryInterface.dropTable('gear');
-
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('gear');
   }
 };
--- migrations/20160831225807-skaters_gear.js
+++ migrations/20160831225807-skaters_gear.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.createTable('skaters_gear',
     {
       id: {
@@ -40,13 +33,6 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return queryInterface.dropTable('skaters_gear')
   }
 };
