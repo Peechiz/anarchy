b7d24d386a67fa9537534c4be8b99818173e5d45
Chris Impicciche <peechiz@bakudan.local>
2016-09-08 13:39:59 -0500

    GET skaters and skater by id

--- api/skaters.js
+++ api/skaters.js
@@ -1,10 +1,36 @@
+const Boom = require('boom');
+
 module.exports = [
   {
     method: 'GET',
     path: '/api/skaters',
     config: {
       handler: function(req,res){
-        res('hello skaters')
+        const Skaters = req.server.app.models.Skaters;
+        Skaters.findAll().then((records)=>{
+          // console.log(records.toJSON());
+          const data = records.map(record => {
+            return record.dataValues
+          })
+          res(data);
+        })
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/skaters/{id}',
+    config: {
+      handler: function(req,res){
+        const Skaters = req.server.app.models.Skaters;
+        Skaters.findById(req.params.id).then(data=>{
+          if (data){
+            res(data.toJSON().getSkater)
+          }
+          else {
+            res(Boom.badRequest('skater not found'))
+          }
+        })
       }
     }
   }
