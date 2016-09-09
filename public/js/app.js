954bf1d90e899890f1c426edd2e8c9c2589b32a3
Chris Impicciche <cimpicci@gmail.com>
2016-09-09 17:42:29 -0500

    adds api factory

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,15 @@
+'use strict'
+
 console.log('app.js loaded');
 var app = angular.module('anarchy',['routes']);
+
+app.factory('api', ['$http', ($http)=>{
+  var api = {};
+  var teams = '/api/teams'
+
+  api.getTeams = function(){
+    return $http.get(teams)
+  }
+
+  return api;
+}])
