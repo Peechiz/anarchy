2ef69c284ad42bfed0bf0abf6d51b849d64dd313
Chris Impicciche <cimpicci@gmail.com>
2016-09-09 14:55:19 -0500

    updates seed to reflect db changes

--- seeders/20160901155127-skaters.js
+++ seeders/20160901155127-skaters.js
@@ -2,47 +2,52 @@
 
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('skaters', [{
-      userName: 'w0mbo',
-      derbyName: 'Wombo Combo',
-      number: '5',
-      team: 'Legion',
-      favPosition: 'Jammer',
-      photo: 'http://www.fillmurray.com/300/300',
-      admin: false,
-      rank: 'member',
-      summary: 'I do it for the dank memes'
-    }, {
-      userName: 'todd',
-      derbyName: '_todd_',
-      number: '22',
-      team: 'Owls',
-      favPosition: 'Pivot',
-      photo: 'http://www.fillmurray.com/300/300',
-      admin: false,
-      rank: 'member',
-      summary: 'this is my summary'
-    }, {
-      userName: 'bruc3',
-      derbyName: 'Bruce Payne',
-      number: '55',
-      team: 'Legion',
-      favPosition: 'Blocker',
-      photo: 'http://www.fillmurray.com/300/300',
-      admin: true,
-      rank: 'captain',
-      summary: 'this is sparta'
-    }, {
-      userName: 'sharkn4do',
-      derbyName: 'Sharknado',
-      number: '111',
-      team: 'Owls',
-      favPosition: 'Pivot',
-      photo: 'http://www.fillmurray.com/300/300',
-      admin: false,
-      rank: 'fresh meat',
-      summary: 'a tornado full of sharks'
-    }]).then(() => {
+    return queryInterface.bulkInsert('teams', [
+      {teamName: 'Legion'},
+      {teamName: 'Owls'}
+    ]).then(()=>{
+      return queryInterface.bulkInsert('skaters', [{
+        userName: 'w0mbo',
+        derbyName: 'Wombo Combo',
+        number: '5',
+        teamId: 1,
+        favPosition: 'Jammer',
+        photo: 'http://www.fillmurray.com/300/300',
+        admin: false,
+        rank: 'member',
+        summary: 'I do it for the dank memes'
+      }, {
+        userName: 'todd',
+        derbyName: '_todd_',
+        number: '22',
+        teamId: 2,
+        favPosition: 'Pivot',
+        photo: 'http://www.fillmurray.com/300/300',
+        admin: false,
+        rank: 'member',
+        summary: 'this is my summary'
+      }, {
+        userName: 'bruc3',
+        derbyName: 'Bruce Payne',
+        number: '55',
+        teamId: 1,
+        favPosition: 'Blocker',
+        photo: 'http://www.fillmurray.com/300/300',
+        admin: true,
+        rank: 'captain',
+        summary: 'this is sparta'
+      }, {
+        userName: 'sharkn4do',
+        derbyName: 'Sharknado',
+        number: '111',
+        teamId: 2,
+        favPosition: 'Pivot',
+        photo: 'http://www.fillmurray.com/300/300',
+        admin: false,
+        rank: 'fresh meat',
+        summary: 'a tornado full of sharks'
+      }])
+    }).then(() => {
       return queryInterface.bulkInsert('brands', [{
         name: 'brand1'
       }, {
@@ -51,129 +56,129 @@ module.exports = {
         name: 'brand3'
       }])
     }).then(() => {
-      return queryInterface.bulkInsert('gear', [{
+      return queryInterface.bulkInsert('gears', [{
         name: 'a_boot',
-        brand_id: 1,
+        brandId: 1,
         type: 'boot'
       }, {
         name: 'a_wheels',
-        brand_id: 2,
+        brandId: 2,
         type: 'wheels'
       }, {
         name: 'a_plates',
-        brand_id: 3,
+        brandId: 3,
         type: 'plates'
       }, {
         name: 'a_toestop',
-        brand_id: 1,
+        brandId: 1,
         type: 'toestop'
       }, {
         name: 'a_helmet',
-        brand_id: 2,
+        brandId: 2,
         type: 'helmet'
       }, {
         name: 'a_kneepads',
-        brand_id: 3,
+        brandId: 3,
         type: 'kneepads'
       }, {
         name: 'a_elbowpads',
-        brand_id: 1,
+        brandId: 1,
         type: 'elbowpads'
       }, {
         name: 'a_wristgaurds',
-        brand_id: 2,
+        brandId: 2,
         type: 'wristgaurds'
       }, {
         name: 'a_mouthgaurd',
-        brand_id: 3,
+        brandId: 3,
         type: 'mouthgaurd'
       }])
     }).then(() => {
-      return queryInterface.bulkInsert('skaters_gear', [{
-        skater_id: 1,
-        gear_id: 2,
-        is_current: true,
+      return queryInterface.bulkInsert('skaters_gears', [{
+        skaterId: 1,
+        gearId: 2,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 1,
-        gear_id: 1,
-        is_current: true,
+        skaterId: 1,
+        gearId: 1,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 1,
-        gear_id: 5,
-        is_current: true,
+        skaterId: 1,
+        gearId: 5,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 1,
-        gear_id: 6,
-        is_current: true,
+        skaterId: 1,
+        gearId: 6,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 1,
-        gear_id: 7,
-        is_current: true,
+        skaterId: 1,
+        gearId: 7,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 1,
-        gear_id: 8,
-        is_current: true,
+        skaterId: 1,
+        gearId: 8,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 1,
-        gear_id: 9,
-        is_current: true,
+        skaterId: 1,
+        gearId: 9,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, { // END SKATER 1
-        skater_id: 2,
-        gear_id: 1,
-        is_current: true,
+        skaterId: 2,
+        gearId: 1,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 3,
-        gear_id: 3,
-        is_current: true,
+        skaterId: 3,
+        gearId: 3,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }, {
-        skater_id: 4,
-        gear_id: 4,
-        is_current: true,
+        skaterId: 4,
+        gearId: 4,
+        isCurrent: true,
         img: 'http://flattrackstats.com/sites/default/files/imagecache/profile/logos/AA-logo.png'
       }])
     }).then(() => {
       return queryInterface.bulkInsert('reviews', [{
-        skater_id: 1,
-        gear_id: 2,
+        skaterId: 1,
+        gearId: 2,
         text: 'i love it',
         rating: 11,
       }, {
-        skater_id: 1,
-        gear_id: 1,
+        skaterId: 1,
+        gearId: 1,
         text: 'i hate it',
         rating: 0,
       }, {
-        skater_id: 1,
-        gear_id: 5,
+        skaterId: 1,
+        gearId: 5,
         text: 'i meh it',
         rating: 5
       }, {
-        skater_id: 1,
-        gear_id: 6,
+        skaterId: 1,
+        gearId: 6,
         text: 'i love it',
         rating: 8,
       }, {
-        skater_id: 1,
-        gear_id: 7,
+        skaterId: 1,
+        gearId: 7,
         text: 'i love it',
         rating: 9,
       }, {
-        skater_id: 1,
-        gear_id: 8,
+        skaterId: 1,
+        gearId: 8,
         text: 'i love it',
         rating: 10,
       }, {
-        skater_id: 1,
-        gear_id: 9,
+        skaterId: 1,
+        gearId: 9,
         text: 'i love it',
         rating: 11,
       }])
