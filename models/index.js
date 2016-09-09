b7de455cc3f0cadf51f29575e1bfe8ed7a312a70
Chris Impicciche <cimpicci@gmail.com>
2016-09-09 14:52:23 -0500

    changes name of files to match plurarlization

--- models/index.js
+++ models/index.js
@@ -6,29 +6,33 @@ var db = require('../db/sequelize');
 console.log('loading models...');
 var models =  {
   Skaters: db.import(__dirname + '/' + 'skaters'),
-  SkatersGear: db.import(__dirname + '/' + 'skaters_gear'),
+  SkatersGears: db.import(__dirname + '/' + 'skaters_gears'),
   Reviews: db.import(__dirname + '/' + 'reviews'),
-  Gear: db.import(__dirname + '/' + 'gear'),
-  Brands: db.import(__dirname + '/' + 'brands')
+  Gears: db.import(__dirname + '/' + 'gears'),
+  Brands: db.import(__dirname + '/' + 'brands'),
+  Teams: db.import(__dirname + '/' + 'teams')
 };
 
 // create relationships between models.
 console.log('creating relationships...');
 (function (m) {
-  m.Skaters.hasMany(m.SkatersGear);
-  m.SkatersGear.belongsTo(m.Skaters);
+  m.Teams.hasMany(m.Skaters);
+  m.Skaters.belongsTo(m.Teams);
 
-  m.Gear.hasMany(m.SkatersGear);
-  m.SkatersGear.belongsTo(m.Gear);
+  m.Skaters.hasMany(m.SkatersGears,{foreignKey: 'skaterId'});
+  m.SkatersGears.belongsTo(m.Skaters,{foreignKey: 'skaterId'});
 
-  m.Brands.hasMany(m.Gear)
-  m.Gear.belongsTo(m.Brands)
+  m.Gears.hasMany(m.SkatersGears);
+  m.SkatersGears.belongsTo(m.Gears);
+
+  m.Brands.hasMany(m.Gears)
+  m.Gears.belongsTo(m.Brands)
 
   m.Skaters.hasMany(m.Reviews)
   m.Reviews.belongsTo(m.Skaters)
 
-  m.Reviews.belongsTo(m.Gear)
-  m.Gear.hasMany(m.Reviews)
+  m.Reviews.belongsTo(m.Gears)
+  m.Gears.hasMany(m.Reviews)
 })(models);
 
 module.exports = models;
--- models/skaters.js
+++ models/skaters.js
@@ -4,7 +4,13 @@
         userName: DataTypes.STRING,
         derbyName: DataTypes.STRING,
         number: DataTypes.STRING,
-        team: DataTypes.STRING,
+        teamId: {
+          type: DataTypes.INTEGER,
+          references: {
+            model: 'Teams',
+            key: 'id'
+          }
+        },
         favPosition: DataTypes.STRING,
         photo: DataTypes.STRING,
         admin: DataTypes.BOOLEAN,
@@ -12,20 +18,20 @@
         summary: DataTypes.TEXT
       }, {
         timestamps: true,
-        getterMethods: {
-          getSkater: function(){
-            return {
-              userName: this.userName,
-              derbyName: this.derbyName,
-              number: this.number,
-              team: this.team,
-              favPosition: this.favPosition,
-              photo: this.photo,
-              rank: this.rank,
-              summary: this.summary
-            }
-          }
-        }
+        // getterMethods: {
+        //   getSkater: function(){
+        //     return {
+        //       userName: this.userName,
+        //       derbyName: this.derbyName,
+        //       number: this.number,
+        //       team: this.team,
+        //       favPosition: this.favPosition,
+        //       photo: this.photo,
+        //       rank: this.rank,
+        //       summary: this.summary,
+        //     }
+        //   }
+        // }
       });
       return Skaters;
     };
