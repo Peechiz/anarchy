2897b0ad50baf7cc418181c1d9eb2aeb6c83aad1
Chris Impicciche <cimpicci@gmail.com>
2016-09-12 10:54:55 -0500

    adds jwt auth structure

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 
 const Hapi = require('hapi');
 const Path = require('path');
+require('dotenv').config();
 
-
-// const sequelize = require('db/sequelize');
 
 const server = new Hapi.Server();
 
@@ -12,30 +11,63 @@ server.connection({ port: process.env.PORT || 3000 });
 
 server.app.models = require('./models/index')
 
-server.register(require('inert'), (err) => {
+
+const skaters = require('./api/skaters')
+const auth = require('./api/auth')
+const teams = require('./api/teams')
+// const thing = require('./api/thing')
+
+
+// server.route(admin);
+
+
+// validate assumes token exists
+var validate = function (decoded, req, callback) {
+
+    // do your checks to see if the person is valid
+    // decoded is the JWT(decoded)
+
+    // if ('not valid') {
+    //   return callback(null, false);
+    // }
+    // else {
+    //   return callback(null, true);
+    // }
+};
+
+server.register(require('hapi-auth-jwt2'), err => {
 
   if (err) {
     throw err;
   }
 
-  const skaters = require('./api/skaters')
-  const auth = require('./api/auth')
-  const teams = require('./api/teams')
-  // const thing = require('./api/thing')
+  server.auth.strategy('jwt', 'jwt',
+    { key: process.env.JWTKEY,
+      validateFunc: validate, // validate function defined above
+      verifyOptions: { algorithms: [ 'HS256' ] } // pick a strong algorithm
+    });
 
-  server.route(skaters);
-  server.route(teams);
-  server.route(auth);
-  // server.route(thing);
+    server.route(skaters);
+    server.route(teams);
+    server.route(auth);
+    
+  // server.auth.default('jwt');
+})
+
+server.register(require('inert'), err => {
+
+  if (err) {
+    throw err;
+  }
 
   server.route({
     method: 'GET',
     path: '/{param*}',
     handler: {
-        directory: {
-            path: 'public',
-            listing: false
-        }
+      directory: {
+          path: 'public',
+          listing: false
+      }
     }
 });
 
