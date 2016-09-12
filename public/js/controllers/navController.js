535d70fd0d84ec2e77816123a17ea17ca715aeeb
Chris Impicciche <cimpicci@gmail.com>
2016-09-12 14:34:27 -0500

    adds admin checking, log in/out

--- public/js/controllers/navController.js
+++ public/js/controllers/navController.js
@@ -1,11 +1,11 @@
 'use strict'
 
-app.controller('navController', ['$scope','$location', 'auth', navController])
+app.controller('navController', ['$scope','$location', 'auth', '$localStorage', navController])
 
-function navController($scope, $location, auth){
+function navController($scope, $location, auth, $localStorage){
   const n = this;
 
-  n.hasLogin = false;
+  n.hasLogin = $localStorage.currentUser ? true : false;
   n.form = false;
 
   n.nav = {
@@ -15,6 +15,13 @@ function navController($scope, $location, auth){
     who: false,
     admin: false,
   }
+
+  // n.isAdmin = (function(){
+  //   if (n.hasLogin){
+  //     return $localStorage.currentUser.admin
+  //   }
+  //   return false
+  // })()
 
   // sets the currently clicked link's color to white
   n.setClicked = function(key){
@@ -40,9 +47,19 @@ function navController($scope, $location, auth){
   n.cb = function(result){
     if (result){
       console.log('logged in');
+      n.form = false;
+      n.hasLogin = true;
     }
     else {
       console.log('there was a problem logging in.');
     }
+  }
+
+  n.logout = function(){
+    console.log('logging out');
+    n.auth.Logout()
+    n.hasLogin = false;
+    n.user = ''
+    n.pass = ''
   }
 }
