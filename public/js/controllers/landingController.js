dbb2646be00934f741170f87a11be2f1f81d0b46
Chris Impicciche <cimpicci@gmail.com>
2016-09-12 11:03:24 -0500

    removes temp login function (moves to nav controller

--- public/js/controllers/landingController.js
+++ public/js/controllers/landingController.js
@@ -1,23 +1,8 @@
 'use strict'
 
-app.controller('landingController', landingController)
+app.controller('landingController', ['$location','$http', landingController])
 
-function landingController($http){
+function landingController($location, $http){
   console.log('controller up');
   const l = this;
-
-  l.blam = ()=>{
-    console.log('BLAM!');
-    console.log(l.pass,l.user);
-    $http({
-      method: 'POST',
-      url: '/api/auth',
-      data: {
-        user: l.user,
-        pass: l.pass
-      }
-    }).then(res=>{
-      console.log(res);
-    })
-  }
 }
