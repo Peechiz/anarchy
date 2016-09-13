0fc6a41b88babcd238b0e4fa31d8d87bc5c3bae8
Chris Impicciche <cimpicci@gmail.com>
2016-09-12 21:51:52 -0500

    major overhaul. adds skaters/id/gear for separate gear/review query

--- api/skaters.js
+++ api/skaters.js
@@ -4,11 +4,18 @@ module.exports = [{
   method: 'GET',
   path: '/api/skaters',
   config: {
-    // auth: 'jwt',
+    // auth: 'admin',
     handler: function(req, res) {
-      const Skaters = req.server.app.models.Skaters;
-      Skaters.findAll({
-        attributes: { exclude: ['password'] }
+      const m = req.server.app.models;
+      m.Skaters.findAll({
+        attributes: { exclude: ['password'] },
+        include: [{
+          model: m.Ranks,
+          attributes: ['name']
+        },{
+          model: m.Teams,
+          attributes: ['teamName']
+        }]
       }).then((records) => {
         // console.log(records.toJSON());
         const data = records.map(record => {
@@ -20,73 +27,47 @@ module.exports = [{
   }
 },{
   method: 'GET',
-  path: '/api/skaters/{id}',
+  path: '/api/skaters/{id}/gear',
   config: {
-    handler: function(req,res){
-      const Skaters = req.server.app.models.Skaters;
-      Skaters.findById(req.params.id).then(skater=>{
-        res(skater.toJSON().getSkater)
+    handler: function(req,res) {
+      const m = req.server.app.models;
+      m.SkatersGears.findAll({
+        where: {skaterId: req.params.id},
+        attributes: ['isCurrent'],
+        include: [{
+          model: m.Gears,
+          attributes: ['name', 'type'],
+          include: [{
+            model: m.Brands,
+            attributes: ['name']
+          },{
+            model: m.Reviews,
+            attributes: ['rating', 'text']
+          }]
+        }]
+      }).then(gear=>{
+        res(gear.map(x => x.toJSON()))
       })
     }
   }
 },{
   method: 'GET',
-  path: '/api/skaters/{id}/skaters_gears',
-  handler: function(req,res){
-    const SkatersGears = req.server.app.models.SkatersGears
-    SkatersGears.findAll({
-      where: {skaterId: req.params.id}
-    }).then(data=>{
-      res(data)
-    })
-  }
-},{
-  method: 'GET',
-  path: '/api/profile/{id}',
+  path: '/api/skaters/{id}',
   config: {
-    handler: function(req, res) {
-      const Models = req.server.app.models
-      Models.Skaters.findOne(
-        {
-        attributes: [
-          'userName',
-          'derbyName',
-          'number',
-          'favPosition',
-          'photo',
-          'rank',
-          'summary'],
+    handler: function(req,res){
+      const m = req.server.app.models;
+      m.Skaters.findOne({
+        where: {id: req.params.id},
+        attributes: {exclude: ['password', 'admin']},
         include: [{
-          model: Models.SkatersGears,
-          attributes: ['isCurrent'],
-          where: {
-            skaterId: req.params.id
-          },
-          include: [{
-            model: Models.Gears,
-            attributes: ['name','type'],
-            include: [{
-              model: Models.Brands,
-              attributes: ['name']
-            },{
-              model: Models.Reviews,
-              attributes: ['text','rating']
-            }]
-          }]
+          model: m.Ranks,
+          attributes: ['name']
         },{
-          model: Models.Teams,
+          model: m.Teams,
           attributes: ['teamName']
         }]
-      }
-    ).then(data => {
-        if (data) {
-          // data = data.toJSON();
-          // var out = data.getSkater
-          // out.gear = data.skaters_gears
-          res(data.toJSON())
-        } else {
-          res(Boom.badRequest('skater not found'))
-        }
+      }).then(skater=>{
+        res(skater.toJSON())
       })
     }
   }
