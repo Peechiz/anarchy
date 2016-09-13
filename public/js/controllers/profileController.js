a428a126ad5bf06cc66af3e19326f0203dfdfd8f
Chris Impicciche <cimpicci@gmail.com>
2016-09-12 22:01:24 -0500

    adds profile factory injection

--- public/js/controllers/profileController.js
+++ public/js/controllers/profileController.js
@@ -1,7 +1,12 @@
 'use strict'
 
-app.controller('profileController', [profileController])
+app.controller('profileController', ['profile', profileController])
 
-function profileController(){
-  console.log('controller up');
+function profileController(profile){
+  console.log('profile controller up');
+
+  const p = this;
+
+  p.profile = profile.data;
+  console.log(p.profile);
 }
