c3ba03cb10d9b09aec5e7efc2376b9712490f49f
Chris Impicciche <cimpicci@gmail.com>
2016-09-21 14:48:15 -0500

    adds send join application and displays success message

--- public/js/controllers/joinController.js
+++ public/js/controllers/joinController.js
@@ -1,11 +1,35 @@
 'use strict'
 
-app.controller('joinController', joinController)
+app.controller('joinController', ['api', joinController])
 
-function joinController(){
+function joinController(api){
   const j = this;
 
+  j.show = 'intro'; // intro, questions, thanks
+  j.passMatch = false;
+
   j.submit = function() {
-    console.log('submitted!');
+
+    if (j.password !== j.passwordConfirm){
+      j.passMatch = true;
+      return
+    }
+
+    var data = {
+      xp: j.xp,
+      userName: j.userName,
+      password: j.password,
+      tel: j.tel,
+      email: j.email,
+      hasSkates: j.hasSkates,
+      hasPads: j.hasPads,
+    }
+    api.submitApplication(data).then(result => {
+      // show thank you message
+      console.log(result);
+      j.show = 'thanks';
+    }, error => {
+      console.log('oops');
+    })
   }
 }
